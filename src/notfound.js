72c3180ff12a1daeeffa16f6a7efcb8ef8297de9
sulaimonazeez <olaniyisulaimon221@gmail.com>
2023-10-30 12:38:04 +0100

    commited

---
+++ src/notfound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function NotFound(){
+  return(
+    <div className="mt-5">
+      <h1 className="mt-5 text-center">404 Page Not Found</h1>
+    </div>
+  )
+}
+export default NotFound

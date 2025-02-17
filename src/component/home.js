afd310ab062d98d1cb7f5bffd18c34cf779968c8
Azeez Sulaimon <111203778+sulaimonazeez@users.noreply.github.com>
2025-02-17 07:17:37 -0800

    Update home.js

--- src/component/home.js
+++ src/component/home.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import resume from './static/resumes.png';
+import resume from './static/sulaimonazeezs.pdf';
 import '../App.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';

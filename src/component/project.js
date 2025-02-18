1d6a6fb921ddd26694d8ea76ffe535dbb5983cfa
Azeez Sulaimon <111203778+sulaimonazeez@users.noreply.github.com>
2025-02-18 05:33:21 -0800

    Update project.js

--- src/component/project.js
+++ src/component/project.js
@@ -3,7 +3,7 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import "../App.css";
 
-const Project = ({image1, title, content,link1, link2}) => {
+const Project = ({image1, title, content,link1, link2, onLoad}) => {
   const [isMobile, setIsMobile] = useState(false);
 
   // Determine if the device is mobile by checking screen width
